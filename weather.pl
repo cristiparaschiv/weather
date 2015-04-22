b7720b363fc8b09efd67db706cab03f23f44479c
cristiparaschiv <cristianv.paraschiv@gmail.com>
2015-04-22 11:55:25 +0300

    switched to use forecast api

--- weather.pl
+++ weather.pl
@@ -7,7 +7,7 @@ use LWP::UserAgent;
 use JSON;
 
 my $city = $ARGV[0] // "Iasi,ro";
-my $api_url = "http://api.openweathermap.org/data/2.5/weather?q=" . $city;
+my $api_url = "http://api.openweathermap.org/data/2.5/forecast/daily?q=" . $city . "&mode=json&units=metric&cnt=5";
 
 my $ua = LWP::UserAgent->new;
 my $data;
@@ -22,22 +22,14 @@ if ($response->is_success) {
 my $json = JSON->new;
 my $decoded = $json->decode($data);
 
-my $weather = {
-	'temperature' => $decoded->{main}->{temp} - 273.15,
-	'humidity' => $decoded->{main}->{humidity} . "%",
-	'pressure' => $decoded->{main}->{pressure} . "hpa",
-	'wind_speed' => $decoded->{wind}->{speed} . "m/s",
-	'sunrise' => (localtime($decoded->{sys}->{sunrise}))[2] . ":" . (localtime($decoded->{sys}->{sunrise}))[1],
-	'sunset' => (localtime($decoded->{sys}->{sunset}))[2] . ":" . (localtime($decoded->{sys}->{sunset}))[1],
-};
+$data = $decoded->{list};
+foreach my $entry (@$data) {
+    print "Date: " . scalar localtime $entry->{dt};
+    print "\n  Temperature: " . int($entry->{temp}->{day}) . "\n";
+    print "  Description: " . $entry->{weather}->[0]->{description} . "\n";
+    print "  Humidity: " . $entry->{humidity} . "\n";
+    print "  Pressure: " . $entry->{pressure} . "\n";
+    print "  Wind Speed: " . $entry->{speed} . "\n";
 
-print "$decoded->{name}\n\n";
-print "Temperature: ";
-my $form_temp = sprintf("%.1f", $weather->{temperature});
-print "$form_temp\n";
-print "Humidity: $weather->{humidity}\n";
-print "Pressure: $weather->{pressure}\n";
-print "Wind Speed: $weather->{wind_speed}\n";
-print "Sunrise: $weather->{sunrise}\n";
-print "Sunset: $weather->{sunset}\n";
+}
 
